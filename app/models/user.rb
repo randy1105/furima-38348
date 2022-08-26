217408b9c3b14d274ac9921f531f2f5c62455cc9
randy1105 <kenshirohokuto53@gmail.com>
2022-08-26 12:09:20 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birthday, presence: true
+       
+  # アソシエーションはモデルを実装したタイミングで#を消す。
+  # has_many :items
+  # has_many :orders
+
+  
 end
