7e00e7a92adc7424810a56cdd4a594f736684e0e
randy1105 <kenshirohokuto53@gmail.com>
2022-08-28 00:48:39 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+  with_options presence: true do
+    validates :nickname
     validates :first_name
     validates :last_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
     validates :first_name_kana
     validates :last_name_kana
+    validates :birthday
   end
-
-  validates :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  with_options allow_blank: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name 
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  end
        
   # アソシエーションはモデルを実装したタイミングで#を消す。
   # has_many :items
