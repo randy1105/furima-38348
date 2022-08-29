021559e22fb3b40a225797cf39b7d38ce75e5bbd
randy1105 <kenshirohokuto53@gmail.com>
2022-08-29 12:47:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :product_name
+    validates :product_description
+    validates :category_id
+    validates :status_id
+    validates :freight_id
+    validates :shipping_prefecture_id
+    validates :ship_date_id
+    validates :price
+  end
+
 end
