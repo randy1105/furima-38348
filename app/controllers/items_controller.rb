5b7b1ea9e6c1bff410ff78a2da6901ff5e3f8cda
randy1105 <kenshirohokuto53@gmail.com>
2022-08-29 20:11:00 +0900

    入力内容を保存できるように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :status_id, :freight_id, :shipping_prefecture_id, :ship_date_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,26 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product_name
     validates :product_description
+    validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :image
+  end
+
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :freight
+  belongs_to :shipping_prefecture
+  belongs_to :ship_date
+
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :freight_id
     validates :shipping_prefecture_id
     validates :ship_date_id
-    validates :price
   end
-
 end
