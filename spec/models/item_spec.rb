9ea09a022c056300ffd239063b4d7fe5303e5afb
randy1105 <kenshirohokuto53@gmail.com>
2022-08-30 19:18:22 +0900

    userのテストコードを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,11 +73,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it 'userが紐づいていないと登録できない' do
-        @item.image = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
-      end
       it 'user_idが空だと投稿できない' do
         @item.user_id = nil
         @item.valid?
