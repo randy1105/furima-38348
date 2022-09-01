15207ff3af44d4b530383e83992451eac1939180
randy1105 <kenshirohokuto53@gmail.com>
2022-09-01 14:28:53 +0900

    一部コントローラ内の記述をリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -21,12 +23,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
     if user_signed_in?
-      @item = Item.find(params[:id])
       redirect_to action: :index unless @item.user_id == current_user.id
     else
       redirect_to root_path
@@ -34,7 +34,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,6 +42,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :product_name, :product_description, :category_id, :status_id, :freight_id,
