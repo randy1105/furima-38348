8b39450af53ff6e51119914ee27891c44f9fbc6a
randy1105 <kenshirohokuto53@gmail.com>
2022-09-05 15:55:19 +0900

    商品購入機能の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     if user_signed_in?
       if Order.exists?(item_id: @item.id) || @item.user.id == current_user.id
         redirect_to root_path
@@ -12,12 +13,7 @@ class OrdersController < ApplicationController
     end
   end
 
-  def new
-    @item = Item.find(params[:item_id])
-  end
-
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(address_params)
     if @order_address.valid?
       set_pay
@@ -30,6 +26,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
 
   def address_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :block, :building_name, :phone_number).merge(
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id:user.id, item_id:item.id)
+    sleep 0.1
   end
 
   describe '配送先情報の保存' do
     context '配送先情報の保存ができるとき' do
       it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it '建物名が空でも保存できること' do
+        @order_address.building_name = ""
         expect(@order_address).to be_valid
       end
     end
@@ -26,8 +33,7 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号が空だと保存できないこと' do
         @order_address.post_code = nil
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Post code can't be blank",
-                                                               'Post code is invalid. Include hyphen(-)')
+        expect(@order_address.errors.full_messages).to include("Post code can't be blank")
       end
       it '郵便番号にハイフンがないと保存できないこと' do
         @order_address.post_code = 1_234_567
@@ -61,6 +67,11 @@ RSpec.describe OrderAddress, type: :model do
       end
       it '電話番号が12桁以上あると保存できないこと' do
         @order_address.phone_number = 12_345_678_910_123_111
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が9桁以下だと保存できないこと' do
+        @order_address.phone_number = 12_345_678_9
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
