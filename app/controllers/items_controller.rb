f09076f44aca9651b67e9b3aec854d9a04b0c9c2
randy1105 <kenshirohokuto53@gmail.com>
2022-09-06 10:55:12 +0900

    商品編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if Order.exists?(item_id: @item.id)
+    if user_signed_in?
+      redirect_to root_path if Order.exists?(item_id: @item.id)
+    else
+      redirect_to redirect_to new_user_session_path
+    end
   end
 
   def update
